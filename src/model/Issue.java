412a7ea8ef413fa38485d845adf4130d405aa3b0
duckey <erbstroke@gmail.com>
2026-02-24 13:39:07 +0600

    Add files via upload

--- src/model/Issue.java
+++ src/model/Issue.java
@@ -1,19 +1,24 @@
 package model;
 
 public class Issue implements Comparable<Issue> {
-
     // TODO: declare fields
     private String description;
     private int urgencyLevel; // 1 = most urgent
 
     // TODO: create constructor
-
+    public Issue (String description , int urgencyLevel){
+        this.description=description;
+        this.urgencyLevel=urgencyLevel;
+    }
     // TODO: implement compareTo()
     @Override
     public int compareTo(Issue other) {
         // TODO: implement priority logic
-        return 0;
+        return (this.urgencyLevel- other.urgencyLevel);
     }
 
     // TODO: override toString()
+    public String toString(){
+        return ("description : "+this.description+" , urgency level : "+this.urgencyLevel);
+    }
 }
--- src/model/Student.java
+++ src/model/Student.java
@@ -8,9 +8,28 @@ public class Student {
     private double gpa;
 
     // TODO: create constructor
+    public Student(int id , String name, double gpa){
+        this.id=id;
+        this.name=name;
+        this.gpa=gpa;
+    }
 
     // TODO: create getters
+    public int getId(){
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getGpa() {
+        return this.gpa;
+    }
 
     // TODO: override toString()
+    public String toString(){
+        return ("Name : "+this.name+" , id : "+this.id+" , GPA : "+this.gpa);
+    }
 
 }
